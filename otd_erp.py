2032189dbbba390b2bf24e7ce5cb688f59baf94e
FelipitoSV <famed1017@gmail.com>
2026-06-29 11:33:11 -0500

    Add files via upload

--- otd_erp.py
+++ otd_erp.py
@@ -28,7 +28,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-APP_VERSION = "V2.20 (Sandbox IP)" 
+APP_VERSION = "V2.21 (Sandbox IP)" 
 ADMIN_PASSWORD = "2526"
 BACKUP_DIR = "backups"
 DB_NAME = "hydra_v1.db"
